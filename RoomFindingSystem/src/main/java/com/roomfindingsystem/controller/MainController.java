f37811405fa818cf10e524fe72d618248476d87b
Đào Phúc Thạch <89058186+Thachdp2808@users.noreply.github.com>
2023-10-02 21:24:30 +0700

    update font-end

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/MainController.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/MainController.java
@@ -1,4 +1,21 @@
 package com.roomfindingsystem.controller;
 
+<<<<<<< Updated upstream
 public class MainController {
 }
+=======
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class  MainController {
+    @GetMapping("/")
+    public String getIndex(){
+        return "listing";
+    }
+
+}
+>>>>>>> Stashed changes
