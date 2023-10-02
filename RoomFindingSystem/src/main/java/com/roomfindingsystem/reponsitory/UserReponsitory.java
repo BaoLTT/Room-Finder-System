88b9e685148d36a7adf545f8602ff1ed30fd682f
nguyenhuubinh <binhnhhe153478@fpt.edu.vn>
2023-10-02 15:37:37 +0700

    proẹct base fix

---
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/reponsitory/UserReponsitory.java
@@ -0,0 +1,7 @@
+package com.roomfindingsystem.reponsitory;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserReponsitory {
+}
---
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/service/UserService.java
@@ -0,0 +1,7 @@
+package com.roomfindingsystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
