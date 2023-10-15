182ad23d322efb7a9b3e9dfa2a9ae26245916b3c
Đào Phúc Thạch <89058186+Thachdp2808@users.noreply.github.com>
2023-10-15 16:57:34 +0700

    update HouseList

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/reponsitory/RoomImageRepository.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/reponsitory/RoomImageRepository.java
@@ -4,6 +4,9 @@ import com.roomfindingsystem.entity.RoomImagesEntity;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 @SpringBootApplication
 public interface RoomImageRepository extends CrudRepository<RoomImagesEntity, Long> {
     @Query("SELECT i FROM RoomImagesEntity i WHERE i.imageId = :imageId")
--- RoomFindingSystem/src/main/java/com/roomfindingsystem/reponsitory/UserReponsitory.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/reponsitory/UserReponsitory.java
@@ -1,6 +1,7 @@
 package com.roomfindingsystem.reponsitory;
 import com.roomfindingsystem.entity.UserEntity;
 import com.roomfindingsystem.vo.UserDto;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
+@SpringBootApplication
 public interface UserReponsitory extends CrudRepository<UserEntity, Integer>, JpaRepository<UserEntity, Integer> {
     public UserEntity save(UserEntity user);
     Optional<UserEntity> findByEmail(String email);
