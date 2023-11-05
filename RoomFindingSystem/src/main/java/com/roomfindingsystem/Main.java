7f40277f2df0253956a41033997f687054bc6c8e
BaoLTT <145529480+BaoLTT@users.noreply.github.com>
2023-11-05 17:44:27 +0700

    chinh sua lai cho thang hang phan type va service

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/Main.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/Main.java
@@ -7,29 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-// implements CommandLineRunner
-    @Autowired
-    private FeedbackRepository feedbackRepository;
+
 
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-//    @Override
-//    public void run(String... args) throws Exception {
-//        FeedbackEntity feedbackEntity = new FeedbackEntity();
-//        feedbackEntity.setContent("nghia");
-//        feedbackEntity.setTitle("ok");
-//        feedbackEntity.setHouseId(1);
-//        feedbackEntity.setMemberId(1);
-//        feedbackRepository.save(feedbackEntity);
-//    }
 
 
-//    @Override
-//    public void run(String... args) throws Exception {
-////        feedbackRepository.deleteByHouseIdAndMemberId(2, 1);
-//    }
+
+
 
 }
