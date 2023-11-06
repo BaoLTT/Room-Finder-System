10b7073d0d8c7fb36f62e686b32ddbce07928c46
nguyenhuubinh <binhnhhe153478@fpt.edu.vn>
2023-11-06 10:02:27 +0700

    update application.properties

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/Main.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/Main.java
@@ -1,9 +1,20 @@
 package com.roomfindingsystem;
 
 import com.roomfindingsystem.repository.FeedbackRepository;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.valves.rewrite.RewriteValve;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class Main {
