e7913d9c3d8b0fea9b2f91ca00bc45a8d95d504c
BaoLTT <145529480+BaoLTT@users.noreply.github.com>
2023-11-08 23:41:42 +0700

    save

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/dto/ServiceDto.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/dto/ServiceDto.java
@@ -14,4 +14,9 @@ public class ServiceDto {
    private Integer serviceId;
    private String serviceName;
    private String description;
+
+   public ServiceDto(String serviceName, String description) {
+      this.serviceName = serviceName;
+      this.description = description;
+   }
 }
