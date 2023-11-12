93d432ea302d28020eb6f7e85e73eb14f215e89f
BaoLTT <145529480+BaoLTT@users.noreply.github.com>
2023-11-12 22:15:43 +0700

    update

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/HomeController.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/HomeController.java
@@ -5,6 +5,9 @@ import com.roomfindingsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import com.roomfindingsystem.service.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,20 +25,66 @@ public class HomeController {
     public String getHome(Model model){
         final String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
         UserEntity user = userService.findByEmail(currentUserName).get();
+    private HouseService houseService;
+
+    private RoomService roomService;
 
         SecurityContextHolder.getContext().getAuthentication().isAuthenticated();
         model.addAttribute("currentUserName", currentUserName);
         model.addAttribute("status", SecurityContextHolder.getContext().getAuthentication().getCredentials()=="");
+    private FeedbackService feedbackService;
 
+    private SliderService sliderService;
 
+    private HouseTypeService houseTypeService;
+
+    private ServiceHouseService serviceHouseService;
+
+    private UserService userService;
 
         return "home";
+    public HomeController(HouseService houseService, RoomService roomService,
+                          FeedbackService feedbackService, SliderService sliderService,
+                          HouseTypeService houseTypeService, ServiceHouseService serviceHouseService,
+                          UserService userService){
+        super();
+        this.houseService = houseService;
+        this.roomService = roomService;
+        this.feedbackService  = feedbackService;
+        this.sliderService = sliderService;
+        this.houseTypeService = houseTypeService;
+        this.serviceHouseService = serviceHouseService;
+        this.userService = userService;
     }
 
+    @GetMapping("")
+    public String viewHomepage(
+            final Model model, HttpServletRequest request){
 
+
+
+        model.addAttribute("houses", houseService.viewHouseInHome() );
+        model.addAttribute("housetypes", houseTypeService.findAll() );
+        model.addAttribute("houseservices", serviceHouseService.findAll());
+        model.addAttribute("rooms", roomService.viewRoomInHome());
+        model.addAttribute("feedbacks", feedbackService.viewTop4Home());
+        model.addAttribute("sliders", sliderService.viewTop7Home());
+
+        final String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserEntity user = null;
+        if (userService.findByEmail(currentUserName).isPresent()) {
+             user = userService.findByEmail(currentUserName).get();
+        }
+
+
+        // Lưu user vào session
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user);
 
     @GetMapping("/test")
     public String getHello(){
         return "hello";
+        return "homepage";
     }
+}
 }
