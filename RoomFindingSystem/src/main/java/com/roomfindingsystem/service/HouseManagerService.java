4120f6b0ce7e0f6af8d4db8ed5eed6ae05763b54
Đào Phúc Thạch <89058186+Thachdp2808@users.noreply.github.com>
2023-11-13 10:18:26 +0700

    update HouseManagerVo

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/service/HouseManagerService.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/service/HouseManagerService.java
@@ -1,8 +1,8 @@
 package com.roomfindingsystem.service;
 
+import com.roomfindingsystem.dto.HouseManagerTypeVo;
 import com.roomfindingsystem.entity.HouseImagesEntity;
 import com.roomfindingsystem.entity.HousesEntity;
-import com.roomfindingsystem.vo.HouseManagerTypeVo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
