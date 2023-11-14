ec53aa136700980e82948939dac5f7d267deb9a7
Đào Phúc Thạch <89058186+Thachdp2808@users.noreply.github.com>
2023-11-14 21:16:43 +0700

    update addressService

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/repository/AddressRepository.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/repository/AddressRepository.java
@@ -2,6 +2,7 @@ package com.roomfindingsystem.repository;
 import com.roomfindingsystem.entity.AddressEntity;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -16,5 +17,16 @@ public interface AddressRepository extends CrudRepository<AddressEntity, Integer
     Optional<AddressEntity> findByProvinceIdAndDistrictIdAndWardIdAndAddressDetails(Integer provinceId, Integer districtId, Integer wardId, String addressDetails);
     @Query(value = "SELECT * FROM room_finding_system.address a where a.addressid = ?1 ",nativeQuery = true)
     AddressEntity getAddressById(Integer id);
+
+    @Modifying
+    @Query(value = "UPDATE room_finding_system.address " +
+            "SET " +
+            "address_details = ?1 ," +
+            "districtid = ?2 ," +
+            "name = ?3 ," +
+            "provinceid = ?4 ," +
+            "wardid = ?5 " +
+            "WHERE addressid = ?6 ;",nativeQuery = true)
+    void updateAddress(String addressDetails, Integer district, String name, Integer province, Integer Ward, Integer id);
 
 }
