125ee3206737609e5be21c46ab9b563bec4416e7
Đào Phúc Thạch <89058186+Thachdp2808@users.noreply.github.com>
2023-11-21 00:32:27 +0700

    update status

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/service/ServiceDetailService.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/service/ServiceDetailService.java
@@ -1,9 +1,12 @@
 package com.roomfindingsystem.service;
 
 import com.roomfindingsystem.dto.ServiceDto;
+import com.roomfindingsystem.entity.ServiceDetailEntity;
+
+import java.util.List;
 
 public interface ServiceDetailService {
     ServiceDto findByName(String name);
 
-
+    List<ServiceDetailEntity> getAllService();
 }
