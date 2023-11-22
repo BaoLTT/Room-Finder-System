b2823c435764e8ef19c521eed2d1e79f1bb99ecc
nguyenhuubinh <binhnhhe153478@fpt.edu.vn>
2023-11-22 23:31:37 +0700

    favourite list

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/repository/FavouriteRepository.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/repository/FavouriteRepository.java
@@ -1,14 +1,17 @@
 package com.roomfindingsystem.repository;
 
+import com.roomfindingsystem.dto.FavouriteDto;
 import com.roomfindingsystem.entity.FavouriteEntity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,8 +20,18 @@ public interface FavouriteRepository extends JpaRepository<FavouriteEntity,Integ
     FavouriteEntity save(FavouriteEntity favouriteEntity);
 
 
-    @PersistenceContext
-    EntityManager em=null;
-    @Query(value = "INSERT INTO `room_finding_system`.`favourite` (`created_date`, `houseid`, `roomid`, `userid`, `status`) VALUES (CURDATE(), ?1, '1', '1', '1');\n", nativeQuery = true)
-    FavouriteEntity insert(int id);
+    @Query("select new com.roomfindingsystem.dto.FavouriteDto(h.houseId,h.houseName,t.typeName, ad.addressDetails, pr.name,d.name,w.name) from FavouriteEntity f " +
+            "join HousesEntity h on f.houseId = h.houseId " +
+            "join TypeHouseEntity t on h.typeHouseId = t.typeId " +
+            "join AddressEntity ad on h.addressId = ad.addressId " +
+            "join ProvinceEntity pr on ad.provinceId = pr.provinceId " +
+            "join DistrictEntity d on ad.districtId = d.districtId " +
+            "join  WardEntity w on ad.wardId = w.wardId " +
+            "order by f.createdDate desc ")
+    List<FavouriteDto> findAllFavourite();
+    @Transactional
+    @Modifying
+    void deleteFavouriteEntitiesByHouseId(int houseid);
+
+    Optional<FavouriteEntity> getAllByHouseId(int houseid);
 }
