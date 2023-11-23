b9de47ec95b798df2491c8b9e2043f15267200cf
ManhHDHE153488 <manhhdhe153488@fpt.edu.vn>
2023-11-23 11:21:49 +0700

    delete copy and export with pdf function.

--- RoomFindingSystem/src/main/resources/static/assets_admin/js/pages/datatables.init.js
+++ RoomFindingSystem/src/main/resources/static/assets_admin/js/pages/datatables.init.js
@@ -17,6 +17,6 @@
 $(document).ready(function () {
     $("#datatable").DataTable(), $("#datatable-buttons").DataTable({
         lengthChange: !1,
-        buttons: ["copy", "excel", "pdf"]
+        buttons: ["excel"]
     }).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)")
 });
