0ba6c0cb66391fec889e2e8cbc38a6e22995a3ae
Đào Phúc Thạch <89058186+Thachdp2808@users.noreply.github.com>
2023-11-24 01:49:00 +0700

    Update ServiceHouseRepository.java

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/repository/ServiceHouseRepository.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/repository/ServiceHouseRepository.java
@@ -3,10 +3,16 @@ package com.roomfindingsystem.repository;
 import com.roomfindingsystem.entity.ServiceDetailEntity;
 import com.roomfindingsystem.entity.ServiceHouseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository("serviceRepository")
 public interface ServiceHouseRepository extends JpaRepository<ServiceHouseEntity, Integer> {
+    @Modifying
+    @Query(value = "DELETE FROM room_finding_system.service_house WHERE houseid= ?1 ",nativeQuery = true)
+    void deleteByHouseId(int houseid);
+
 }
