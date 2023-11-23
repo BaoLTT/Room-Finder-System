a669c796495840b6a9155da5b44e286c821f27b7
ManhHDHE153488 <manhhdhe153488@fpt.edu.vn>
2023-11-23 19:19:43 +0700

    delete function not use

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/RoomController.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/RoomController.java
@@ -24,12 +24,6 @@ public class RoomController {
     private HouseService houseService;
     @Autowired
     private UserService userService;
-    @Autowired
-    private ServiceRoomService serviceRoomService;
-    @Autowired
-    private RoomTypeService roomTypeService;
-    @Autowired
-    private ServiceDetailService serviceDetailService;
 
     @GetMapping("/{id}")
     public String getRoom(Model model, @PathVariable("id") int id){
@@ -47,85 +41,5 @@ public class RoomController {
     public String getRoomInHouseDetail(Model model, @RequestParam(name = "roomId") String id){
         int id1 = Integer.parseInt(id);
         return "redirect:/room/"+id1;
-    }
-
-    @GetMapping("/listRoomPage")
-    public String getListRoomPage(Model model) {
-        List<RoomDto> roomDtos = roomService.getAll();
-        model.addAttribute("rooms", roomDtos);
-        return "/list-room";
-    }
-
-    @GetMapping("/updateRoom/{id}")
-    public String getFormUpdateRoom(@PathVariable("id") Integer id, Model model){
-        RoomDto roomDto = roomService.findById(id);
-        model.addAttribute("room", roomDto);
-        System.out.println(roomDto);
-        model.addAttribute("types", roomTypeService.findAll());
-        return "edit-room";
-    }
-
-    @PostMapping("/update")
-    public String update(@ModelAttribute(name = "room") RoomDto roomDto, @RequestParam("file") MultipartFile[] files) throws IOException {
-        List<ServiceDto> serviceDtos = new ArrayList<>();
-        List<String> selects = roomDto.getServiceNames();
-        System.out.println(files.length);
-        if (selects != null) {
-            for (String serviceName : selects) {
-                ServiceDto serviceDto = new ServiceDto();
-                serviceDto.setServiceName(serviceName);
-                System.out.println(serviceName);
-                serviceDto.setServiceId(serviceDetailService.findByName(serviceName).getServiceId());
-                serviceDtos.add(serviceDto);
-            }
-        }
-        roomDto.setServiceDtos(serviceDtos);
-        roomService.update(roomDto, files);
-        return "redirect:/room/updateRoom/" + roomDto.getRoomId();
-    }
-
-    @GetMapping("/deleteRoom/{id}")
-    public String delete(@PathVariable("id") Integer id, Model model){
-        roomService.deleteById(id);
-        return "redirect:/room/listRoomPage";
-    }
-    @GetMapping("/insertRoomPage")
-    public String insertRoomPage(Model model) {
-        RoomDto roomDto = new RoomDto();
-        model.addAttribute("room", roomDto);
-        model.addAttribute("services", serviceRoomService.findAll());
-        model.addAttribute("types", roomTypeService.findAll());
-        return "insert-room";
-    }
-
-    @PostMapping("/save")
-    public String save(@ModelAttribute(name = "room") RoomDto roomDto, @RequestParam("file") MultipartFile[] files) throws IOException {
-        try {
-            List<ServiceDto> serviceDtos = new ArrayList<>();
-            List<String> selects = roomDto.getServiceNames();
-            for (String serviceName : selects) {
-                ServiceDto serviceDto = new ServiceDto();
-                serviceDto.setServiceName(serviceName);
-                System.out.println(serviceName);
-                serviceDto.setServiceId(serviceDetailService.findByName(serviceName).getServiceId());
-                serviceDtos.add(serviceDto);
-            }
-            System.out.println(serviceDtos);
-            roomDto.setServiceDtos(serviceDtos);
-            roomService.save(roomDto, files);
-        } catch (Exception ex) {
-        }
-        return "redirect:/room/listRoomPage";
-    }
-
-    @PostMapping("/importRooms")
-    public String importRoom(@RequestParam("fileExcel") MultipartFile fileExcel) {
-        roomService.importRooms(fileExcel);
-        return "redirect:/room/listRoomPage";
-    }
-    @GetMapping("deleteImage/{roomId}/{imageId}")
-    public String deleteImage(@PathVariable Integer roomId, @PathVariable Integer imageId) {
-        roomService.deleteRoomImage(imageId);
-        return "redirect:/room/updateRoom/" + roomId;
     }
 }
