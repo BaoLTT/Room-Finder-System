623ffba4de53b734be827c8d0447dec93e5f7c47
nguyenhuubinh <binhnhhe153478@fpt.edu.vn>
2023-11-25 00:03:02 +0700

    update favourite list

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/FavouriteController.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/FavouriteController.java
@@ -2,12 +2,17 @@ package com.roomfindingsystem.controller;
 
 import com.roomfindingsystem.dto.FavouriteDto;
 import com.roomfindingsystem.entity.FavouriteEntity;
+import com.roomfindingsystem.entity.UserEntity;
 import com.roomfindingsystem.service.FavouriteService;
+import com.roomfindingsystem.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -26,11 +31,19 @@ import java.util.List;
 public class FavouriteController {
     @Autowired
     FavouriteService favouriteService;
+    private UserService userService;
+
 
 
     @RequestMapping(value = "favourite-list")
-    public String getFavourite(Model model) {
-        List<FavouriteDto> list = favouriteService.getListFavourite(2);
+    public String getFavourite(Model model, HttpSession session, HttpServletRequest request) {
+        session = request.getSession();
+        UserEntity user = (UserEntity) session.getAttribute("user");
+
+        List<FavouriteDto> list = favouriteService.getListFavourite(user.getUserId());
+        if (list.isEmpty()){
+            return "favourite-null";
+        }
         System.out.println(list);
         model.addAttribute("listFavourite",list);
         return "favourite-list";
@@ -45,13 +58,14 @@ public class FavouriteController {
 
 
     @RequestMapping(value = "add-favourite-list")
-    public String addToFavourite(@RequestParam("id") Integer id, Model model) {
-        if (!favouriteService.getAllByHouseId(2,id).isPresent()){
+    public String addToFavourite(@RequestParam("id") Integer id, Model model, HttpSession session, HttpServletRequest request) {
+        session = request.getSession();
+        UserEntity user = (UserEntity) session.getAttribute("user");
+        if (!favouriteService.getAllByHouseId(user.getUserId(),id).isPresent()){
             FavouriteEntity favouriteEntity = new FavouriteEntity();
             LocalDate now = LocalDate.now();
             // get session id
-            favouriteEntity.setUserId(2);
-            favouriteEntity.setRoomId(1);
+            favouriteEntity.setUserId(user.getUserId());
             favouriteEntity.setCreatedDate(now);
             favouriteEntity.setHouseId(id);
             favouriteService.addToFavourite(favouriteEntity);
