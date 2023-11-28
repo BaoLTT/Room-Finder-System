d181b139ee406852e9de691e419d341bf3c2e551
BaoLTT <145529480+BaoLTT@users.noreply.github.com>
2023-11-28 19:12:01 +0700

    revert lai

--- RoomFindingSystem/src/test/java/com/example/roomfindingsystem/service/UserRegisterTest.java
+++ RoomFindingSystem/src/test/java/com/example/roomfindingsystem/service/UserRegisterTest.java
@@ -128,7 +128,10 @@ public class UserRegisterTest {
         String originalEmail = "baoltthe153367@fpt.edu.vn";
         UserEntity userEntity = new UserEntity();
         userEntity.setEmail(originalEmail);
+<<<<<<< HEAD
         //giả định
+=======
+>>>>>>> parent of d28a035 (update map)
         when(userRepository.findByEmail(originalEmail)).thenReturn(Optional.of(userEntity));
 
         String uppercaseEmail = "BAOLTTHE153367@FPT.EDU.VN";
