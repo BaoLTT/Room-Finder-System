8b6e3c432fd3c828c28152cafee91a5fe41d46ac
BaoLTT <145529480+BaoLTT@users.noreply.github.com>
2023-11-29 12:02:50 +0700

    them slider detail

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/SliderController.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/controller/SliderController.java
@@ -1,14 +1,28 @@
 package com.roomfindingsystem.controller;
 
+import com.roomfindingsystem.entity.SliderEntity;
+import com.roomfindingsystem.service.SliderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.format.DateTimeFormatter;
 
 @RequestMapping("/slider")
 @Controller
 public class SliderController {
-    @GetMapping
-    public String view(){
+    @Autowired
+    SliderService sliderService;
+    @GetMapping("/{id}")
+    public String view(Model model,@PathVariable("id") int id){
+        SliderEntity slider = sliderService.getSliderById(id);
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//        String formattedDate = slider.getCreatedDate().format(formatter);
+//        model.addAttribute("formattedDate", formattedDate);
+        model.addAttribute("slider", slider);
         return "slider-detail";
     }
 }
