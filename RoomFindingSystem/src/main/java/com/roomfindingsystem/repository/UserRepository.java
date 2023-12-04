1abc9b25e44cd9930c0b65ec6e422ed279d7b46c
BinhNHHE153478 <binhnhhe153478@fpt.edu.vn>
2023-12-04 18:50:11 +0700

    update test for user controller

--- RoomFindingSystem/src/main/java/com/roomfindingsystem/repository/UserRepository.java
+++ RoomFindingSystem/src/main/java/com/roomfindingsystem/repository/UserRepository.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity,Integer> {
     public UserEntity save(UserEntity user);
     Optional<UserEntity> findByEmail(String email);
